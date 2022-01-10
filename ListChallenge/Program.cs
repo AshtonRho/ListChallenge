358314862eb822ca9f96761493f07611e4ba46d8
Ashton Rhodes <16RhodeA@fulford.york.sch.uk>
2022-01-10 13:38:01 +0000

    close #1 close #2 close #3

--- ListChallenge/Program.cs
+++ ListChallenge/Program.cs
@@ -10,10 +10,15 @@ namespace ListChallenge
     {
         static void Main(string[] args)
         {
-            List<string> names = new List<string> { "Will 1", "Will 2", "Ethan"};
-            names.Add("Tom");
+            List<string> names = new List<string> { "Will 1", "Will 2", "Ethan", "Ashton", "Ben", "Tom", "Henry", "Cormac", "Adam" };
+            List<int> scores = new List<int> { 9, 2, 3, 10, 5, 6, 7, 4, 8, };
 
-            Console.WriteLine(names[3]);
+
+            for (int n = 0; n < 9; n++)
+            {
+                Console.WriteLine(names[n] + " = " + scores[n]);
+            }
+            Console.ReadLine();
         }
     }
 }
